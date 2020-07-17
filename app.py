38dd72e70f3151d5ee54fcd80d739c653d38d9e6
Srikar Mudili <srikarm@Srikars-MacBook-Pro.local>
2020-07-16 19:09:44 -0500

    more progress

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from flask import Flask, send_from_directory
-from flask import jsonify
+from flask import jsonify 
 from flask_cors import CORS
+from flask import request
 
 # creates a Flask application, named app
 app = Flask(__name__, static_folder='static', static_url_path='')
 CORS(app)
 
-f = open("resume.json", "r")
-taha = f.read()
 
-@app.route("/")
-def index():
-    return send_from_directory('static', 'index.html')
+@app.route("/api/location/search")
+def get_WOEID():
+    query = request.args.get('query')
+    return f"getWOEID {query}"
 
-# a route where we will display a welcome message via an HTML template
-@app.route("/resume")
-def resume():
-    return taha
+
+@app.route("/api/location/<woeid>")
+def get_weather(woeid):
+    return f"get_weather {woeid}"
+
 
 # run the application
 if __name__ == "__main__":
