1d9ea8cbe51c29675441edd7d63f2624690e7fbf
Srikar Mudili <srikarm@Srikars-MacBook-Pro.local>
2020-07-16 20:32:15 -0500

    more progress

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def get_WOEID():
     return response.text
 
 
-@app.route("/api/location/<woeid>")
+@app.route("/api/location/<woeid>/")
 def get_weather(woeid):
     response = requests.get(f"https://www.metaweather.com/api/location/{woeid}")
     return response.text
